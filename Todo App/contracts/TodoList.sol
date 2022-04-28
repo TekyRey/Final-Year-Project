ed70a70ea3b00ee8f8244d80c452b18756d3e79f
TekyRey <mwakabayah@gmail.com>
2022-04-28 17:21:23 +0300

    updates

---
+++ Todo App/contracts/TodoList.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.5.0;
+
+contract TodoList{
+    uint public taskCount = 0;
+}
+
+
+
+
+//create smart contract for todo list with name,date,description and store in blockchain
+// contract Todolist{
+//     //create array of structs
+//     struct Task{
+//         string name;
+//         string date;
+//         string description;
+//     }
+//     //create array of structs
+//     Task[] public tasks;
+//     //create variable to store number of tasks
+//     uint public taskCount = 0;
+//     //create variable to store number of tasks
+//     uint public completedCount = 0;
+//     //create variable to store number of tasks
+//     uint public uncompletedCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalCount = 0;
+//     //create variable to store number of tasks
+//     uint public completedTotalCount = 0;
+//     //create variable to store number of tasks
+//     uint public uncompletedTotalCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalTaskCount = 0;
+//     //create variable to store number of tasks
+//     uint public completedTotalTaskCount = 0;
+//     //create variable to store number of tasks
+//     uint public uncompletedTotalTaskCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalCompletedTaskCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalUncompletedTaskCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalCompletedCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalUncompletedCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalCompletedTotalCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalUncompletedTotalCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalTaskCompletedCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalTaskUncompletedCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalTaskCompletedTotalCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalTaskUncompletedTotalCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalTaskCompletedTotalTaskCount = 0;
+//     //create variable to store number of tasks
+//     uint public totalTaskUncompletedTotalTaskCount = 0;
+//     //create variable
+// }
+
+
